101630e89e8ef2658ecf205f514f35d701488ab9
MoguchiyDuh <kirnekrasov12@gmail.com>
2026-01-23 08:43:07 +0100

    add suda.vim for sudo editing

--- dotfiles/nvim/.config/nvim/plugin/packer_compiled.lua
+++ dotfiles/nvim/.config/nvim/plugin/packer_compiled.lua
@@ -213,6 +213,12 @@ _G.packer_plugins = {
     path = "/home/kirill/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["suda.vim"] = {
+    config = { "\27LJ\2\n1\0\0\2\0\3\0\0056\0\0\0009\0\1\0)\1\1\0=\1\2\0K\0\1\0\20suda_smart_edit\6g\bvim\0" },
+    loaded = true,
+    path = "/home/kirill/.local/share/nvim/site/pack/packer/start/suda.vim",
+    url = "https://github.com/lambdalisue/suda.vim"
+  },
   ["telescope-fzf-native.nvim"] = {
     loaded = true,
     path = "/home/kirill/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
@@ -240,6 +246,10 @@ time([[Config for which-key.nvim]], false)
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: suda.vim
+time([[Config for suda.vim]], true)
+try_loadstring("\27LJ\2\n1\0\0\2\0\3\0\0056\0\0\0009\0\1\0)\1\1\0=\1\2\0K\0\1\0\20suda_smart_edit\6g\bvim\0", "config", "suda.vim")
+time([[Config for suda.vim]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
